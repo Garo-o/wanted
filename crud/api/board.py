221a1299a747b24d9342d5c7b7d43cba58eaf462
Garo-o <hjs8482@naver.com>
2021-10-27 03:25:12 +0900

    update/controller

--- crud/api/board.py
+++ crud/api/board.py
@@ -2,29 +2,54 @@ from flask import jsonify
 from flask_restful import Resource, request
 from db.boardRepository import *
 from models.board import Board
+from flask_jwt_extended import get_jwt, get_jwt_identity, jwt_required
+from utils import role
 
 
-def isExistBoard(self, by_id):
+def isExistBoard(by_id):
     if by_id == None:
         raise Exception('해당 게시글이 존재하지 않습니다.')
+
+
+def isAdmin(jwt):
+    roles = jwt['roles'].split(',')
+    flag = False
+    for r in roles:
+        if r == role.ROLE_ADMIN:
+            flag = True
+    if not flag:
+        raise Exception('권한이 없습니다.')
+
+
+def isMe(email):
+    if not get_jwt_identity() ==email:
+        raise Exception('권한이 없습니다.')
 
 ### http://localhost:5000/board
 class BoardApi1(Resource):
     '''
     new board
+    @USER, ADMIN
     '''
+    @jwt_required()
     def post(self):
         title = request.json.get('title')
         context = request.json.get('context')
-        writer = request.json.get('writer')
+        writer = get_jwt_identity()
 
         permit(Board(0, title, context, writer, '', ''))
         return jsonify(result="success")
 
     """
     get all boards
+    @ADMIN
     """
+    @jwt_required()
     def get(self):
+        try:
+            isAdmin(get_jwt())
+        except Exception as e:
+            return jsonify(result="fail", error=e.args[0])
         a_ll = findALl()
         cnt = len(a_ll)
         data = [Board(i[0], i[1], i[2], i[3], i[4], i[5]).json() for i in a_ll]
@@ -34,12 +59,15 @@ class BoardApi1(Resource):
 class BoardApi2(Resource):
     '''
     delete by board id
+    @ADMIN, USER
     '''
-
+    @jwt_required()
     def delete(self, param):
         by_id = findById(param)
         try:
             isExistBoard(by_id)
+            isAdmin(get_jwt())
+            isMe(by_id[3])
         except Exception as e:
             return jsonify(result="fail", error=e.args[0])
 
@@ -58,32 +86,37 @@ class BoardApi2(Resource):
         data = [Board(i[0], i[1], i[2], i[3], i[4], i[5]).json() for i in boards]
         return jsonify(result="success", count=cnt, data=data)
 
-
-### http://localhost:5000/board/update
-class BoardApi3(Resource):
-
     '''
     update board
+    @USER
     '''
+    @jwt_required()
+    def post(self, param):
+        board_ = findById(param)
+        try:
+            isExistBoard(board_)
+            isMe(board_[3])
+        except Exception as e:
+            return jsonify(result="fail", error=e.args[0])
 
-    def post(self):
         title = request.json.get('title')
         context = request.json.get('context')
 
-        update(Board(0, title, context, '', '', ''))
+        update(Board(param, title, context, '', '', ''))
         return jsonify(result="success")
 
 
 ### http://localhost:5000/board/get/<int:param>
-class BoardApi4(Resource):
+class BoardApi3(Resource):
     '''
-    delete by board id
+    find one by id
+    @ADMIN
     '''
-
     def get(self, param):
         one = findById(param)
         try:
             isExistBoard(one)
+            isAdmin(get_jwt())
         except Exception as e:
             return jsonify(result="fail", error=e.args[0])
 
--- crud/api/user.py
+++ crud/api/user.py
@@ -3,16 +3,32 @@ from flask_restful import Resource, request
 import bcrypt
 from db.userRepository import *
 from models.user import User
+from flask_jwt_extended import get_jwt, jwt_required, get_jwt_identity
+from utils import role
 
-
-def checkDuplicate(self, email):
+def checkDuplicate(email):
     if (findByEmail(email)):
         raise Exception('이미 존재하는 email입니다.')
 
 
-def isExistUser(self, by_id):
+def isExistUser(by_id):
     if by_id == None:
         raise Exception('해당 아이디가 존재하지 않습니다.')
+
+def isAdmin(jwt):
+    roles = jwt['roles'].split(',')
+    flag = False
+    for r in roles:
+        if r == role.ROLE_ADMIN:
+            flag = True
+    if not flag:
+        raise Exception('권한이 없습니다.')
+
+
+def isMe(email):
+    if not get_jwt_identity() == email:
+        raise Exception('권한이 없습니다.')
+
 
 ### http://localhost:5000/user
 class UserApi1(Resource):
@@ -27,16 +43,23 @@ class UserApi1(Resource):
             return jsonify(result="fail", error=e.args[0])
         password = request.json.get('password')
         password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        permit(User(0, email, password))
+        permit(User(0, email, ["ROLE_USER"], password))
         return jsonify(result="success")
 
-    """
+    '''
     get all users
-    """
+    @ADMIN
+    '''
+    @jwt_required()
     def get(self):
+        try:
+            isAdmin(get_jwt())
+        except Exception as e:
+            return jsonify(result="fail", error=e.args[0])
+
         a_ll = findALl()
         cnt = len(a_ll)
-        data = [User(i[0], i[1]).json() for i in a_ll]
+        data = [User(i[0], i[1], i[2].split(",")).json() for i in a_ll]
         return jsonify(result="success", count=cnt, data=data)
 
 
@@ -44,11 +67,15 @@ class UserApi1(Resource):
 class UserApi2(Resource):
     '''
     delete by user id
+    @ADMIN, USER
     '''
+    @jwt_required()
     def delete(self, param):
         by_id = findById(param)
         try:
             isExistUser(by_id)
+            isAdmin(get_jwt())
+            isMe(by_id[1])
         except Exception as e:
             return jsonify(result="fail", error=e.args[0])
 
@@ -56,15 +83,19 @@ class UserApi2(Resource):
         return jsonify(result="success")
     '''
     find by user id
+    @ADMIN, USER
     '''
+    @jwt_required()
     def get(self, param):
         by_id = findById(param)
         try:
             isExistUser(by_id)
+            isAdmin(get_jwt())
+            isMe(by_id[1])
         except Exception as e:
             return jsonify(result="fail", error=e.args[0])
 
-        data=User(by_id[0], by_id[1]).json()
+        data = User(by_id[0], by_id[1], by_id[2].split(",")).json()
         return jsonify(result="success", data=data)
 
 
@@ -72,13 +103,15 @@ class UserApi2(Resource):
 class UserApi3(Resource):
     '''
     update user
-    jwt이용해서 아이디 조회로 변경 예쩡
+    @USER
     '''
+    @jwt_required()
     def post(self):
-        email=request.json.get('email')
-        by_email=findByEmail(email)
-        user = User(by_email[0],by_email[1],by_email[2])
-        user.password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+        email = get_jwt_identity()
+        by_email = findByEmail(email)
+        password = request.json.get('password')
+        user = User(by_email[0], by_email[1], by_email[2], by_email[3])
+        user.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
         update(user)
         return jsonify(result="success")
